a5286f73b5dfbdc2ceb7884c5731b5ca17608b9f
Ronit-11 <98330660+Ronit-11@users.noreply.github.com>
2023-10-02 14:01:48 +0300

    code cleaned

--- gDrive_config.py
+++ gDrive_config.py
@@ -9,19 +9,13 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
           'https://www.googleapis.com/auth/drive.file']
 
 creds = None
 def credentialsSetup():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
     global creds
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('gDrive/token.json'):
         creds = Credentials.from_authorized_user_file('gDrive/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -54,18 +48,11 @@ def credentialsSetup():
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
     except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
         print(f'An error occurred: {error}')
 
 def create_folder(folder_Name):
-    """ Create a folder and prints the folder ID
-    Returns : Folder Id
-    """
-    #creds, _ = google.auth.default()
-
     try:
         
-        # create drive api client
         service = build('drive', 'v3', credentials=creds)
         
         file_metadata = {
@@ -73,7 +60,6 @@ def create_folder(folder_Name):
             'mimeType': 'application/vnd.google-apps.folder'
         }
     
-        # pylint: disable=maybe-no-member
         file = service.files().create(body=file_metadata, fields='id').execute()
         print(F'Folder ID: "{file.get("id")}".')
         return file.get('id')
@@ -82,86 +68,70 @@ def create_folder(folder_Name):
         print(F'An error occurred: {error}')
         return None
 
-    def upload_to_folder(folder_id, loc_folder_Name, file_Name, file_Type):
-        """Upload a file to the specified folder and prints file ID, folder ID
-        Args: Id of the folder
-        Returns: ID of the file uploaded
-        """
-        # creds, _ = google.auth.default()
+def upload_to_folder(folder_id, loc_folder_Name, file_Name, file_Type):
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
 
-        try:
-            # create drive api client
-            service = build('drive', 'v3', credentials=creds)
+        file_metadata = {
+            'name': file_Name,
+            'parents': [folder_id]
+        }
+        media = MediaFileUpload((loc_folder_Name + '/' + file_Name),
+                                mimetype=file_Type, resumable=True)
+        # pylint: disable=maybe-no-member
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        print(F'File ID: "{file.get("id")}".')
+        return file.get('id')
 
-            file_metadata = {
-                'name': file_Name,
-                'parents': [folder_id]
-            }
-            media = MediaFileUpload((loc_folder_Name + '/' + file_Name),
-                                    mimetype=file_Type, resumable=True)
-            # pylint: disable=maybe-no-member
-            file = service.files().create(body=file_metadata, media_body=media,
-                                          fields='id').execute()
-            print(F'File ID: "{file.get("id")}".')
-            return file.get('id')
-
-        except HttpError as error:
-            print(F'An error occurred: {error}')
-            return None
-
-    def search(mimeType, fileName):
-        """Search file in drive location
-        """
-        # creds, _ = google.auth.default()
-
-        try:
-            # create drive api client
-            service = build('drive', 'v3', credentials=creds)
-            files = []
-            page_token = None
-            while True:
-                # pylint: disable=maybe-no-member
-                response = service.files().list(q="mimeType='" + mimeType + "'",
-                                                spaces='drive',
-                                                fields='nextPageToken, '
-                                                       'files(id, name)',
-                                                pageToken=page_token).execute()
-                # for file in response.get('files', []):
-                # Process change
-                #   print(F'Found file: {file.get("name")}, {file.get("id")}')
-                files.extend(response.get('files', []))
-                page_token = response.get('nextPageToken', None)
-                if page_token is None:
-                    break
-
-        except HttpError as error:
-            print(F'An error occurred: {error}')
-            files = None
-
-        for file in files:
-            if file.get('name') == fileName:
-                return file.get("id")
-
+    except HttpError as error:
+        print(F'An error occurred: {error}')
         return None
 
+def search(mimeType, fileName):
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+        files = []
+        page_token = None
+        while True:
+            # pylint: disable=maybe-no-member
+            response = service.files().list(q="mimeType='" +mimeType + "'",
+                                            spaces='drive',
+                                            fields='nextPageToken, '
+                                                   'files(id, name)',
+                                            pageToken=page_token).execute()
+            
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        files = None
+
+    for file in files:
+        if file.get('name') == fileName:
+            return file.get("id")
+
+    return None
 
 def dirFilesUploader(dir_Name, file_Mime):
     folder_Mime = 'application/vnd.google-apps.folder'
     folder = None
-
+    
     folder = search(folder_Mime, dir_Name)
     if folder == None:
         folder = create_folder(dir_Name)
-
+    
     directory = os.fsencode(dir_Name)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        # print(os.path.join(directory, filename))
         files = search(file_Mime, filename)
         if files == None:
             upload_to_folder(folder, dir_Name, filename, file_Mime)
-        # print(filename)
-
 
 def main():
     credentialsSetup()
@@ -173,7 +143,6 @@ def main():
     jsonl_Mime = 'application/jsonl'
     dir1_Name = 'jsonl_files'
     dirFilesUploader(dir1_Name, jsonl_Mime)
-
 
 if __name__ == '__main__':
     main()
--- generator.sh
+++ generator.sh
@@ -4,13 +4,16 @@ pcname="ronit"
 venv="pyCodelabs"
 projectlocation="Desktop/Python/CAT1_CompGraphics"
 pythonFile="main.py"
+#functiontocall="en_XX_fileGenerator"
 
 cd /c/Users/$pcname/$venv
-#sleep 3
+
 source ./Scripts/Activate
 cd /c/Users/$pcname/$projectlocation
 python $pythonFile
+
+#to run a function of a file:
+#python -c "from $pythonFile import $functiontocall; $functiontocall()"
+
 # Deactivate the virtualenv
 deactivate
-
-#python /path/to/python/script.py
