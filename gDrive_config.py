237bffd57d50ed84c353c3debfad4669a63496f4
sandra-nthenya <91523803+sandra-nthenya@users.noreply.github.com>
2023-10-02 12:23:42 +0300

    edit libraries

--- gDrive_config.py
+++ gDrive_config.py
@@ -6,6 +6,8 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
@@ -131,7 +133,7 @@ def create_folder(folder_Name):
                 page_token = response.get('nextPageToken', None)
                 if page_token is None:
                     break
-                    
+
         except HttpError as error:
             print(F'An error occurred: {error}')
             files = None
